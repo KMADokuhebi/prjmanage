5adca60db2dfc13c15e109371ef164eaff7985e6
kmadokuhebi <kmadokuhebi@gmail.com>
2021-06-17 15:55:20 +0700

    up

---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'React'
+const Login = () => {
+
+    const [state, setstate] = useState(initialState)
+
+
+    return (
+        console.log('state')
+    )
+}
+export default Login
